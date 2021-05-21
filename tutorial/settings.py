efdf63878096062ba88f7247bfc52fc2ed2b8537
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-21 04:23:07 -0700

    added pipeline to check for duplication and store in sqlite local database

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -62,9 +62,11 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'tutorial.pipelines.TutorialPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   # 'tutorial.pipelines.TutorialPipeline': 300,
+   'tutorial.pipelines.DuplicatesPipeline':100,
+   'tutorial.pipelines.SaveQuotesPipeline': 301,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -86,3 +88,6 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+CONNECTION_STRING = 'sqlite:///scrapy_quotes.db'
