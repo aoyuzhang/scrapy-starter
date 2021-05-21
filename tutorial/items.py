2659555ecfa0c938b4cf0800f26c9af2b1a817ba
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-21 02:42:01 -0700

    started to  make use of the items.py file to process the collected data

--- tutorial/items.py
+++ tutorial/items.py
@@ -4,9 +4,22 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
+from scrapy.loader.processors import MapCompose
 
+def remove_quotes(text):
+    text = text.strip(u'\u201c'u'\u201d')
+    return text
 
-class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class QuoteItem(Item):
+    quote_content = Field()
+    tags = Field()
+    author_name = Field()
+    author_birthday = Field()
+    author_bornlocation = Field()
+    author_bio = Field()
+
+# class TutorialItem(scrapy.Item):
+#     # define the fields for your item here like:
+#     # name = scrapy.Field()
+#     pass
