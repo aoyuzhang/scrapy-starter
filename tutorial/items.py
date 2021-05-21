06ffb79f1d019f09a30259d17d3c75268357ec7b
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-21 03:30:53 -0700

    Finish items.py file and updated quotes-spider.py file

--- tutorial/items.py
+++ tutorial/items.py
@@ -5,20 +5,41 @@
 
 import scrapy
 from scrapy.item import Item, Field
-from scrapy.loader.processors import MapCompose
+from scrapy.loader.processors import MapCompose, TakeFirst
+from datetime import datetime
 
 def remove_quotes(text):
     text = text.strip(u'\u201c'u'\u201d')
     return text
 
-class QuoteItem(Item):
-    quote_content = Field()
-    tags = Field()
-    author_name = Field()
-    author_birthday = Field()
-    author_bornlocation = Field()
-    author_bio = Field()
+def convert_date(text):
+    return datetime.strptime(text, '%B %d, %Y')
 
+def parse_location(text):
+    return text[3:]
+
+class QuoteItem(Item):
+    quote_content = Field(
+        input_processor = MapCompose(remove_quotes),
+        output_processor = TakeFirst()
+    )
+    author_name = Field(
+        input_processor = MapCompose(str.strip),
+        output_processor = TakeFirst()
+    )
+    author_birthday = Field(
+      input_processor = MapCompose(convert_date),
+      output_processor = TakeFirst()
+    )
+    author_bornlocation = Field(
+        input_processor = MapCompose(parse_location),
+        output_processor = TakeFirst()
+    )
+    author_bio = Field(
+        input_processor = MapCompose(str.strip),
+        output_processor = TakeFirst()
+    )
+    tags = Field()
 # class TutorialItem(scrapy.Item):
 #     # define the fields for your item here like:
 #     # name = scrapy.Field()
